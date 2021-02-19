1a571df170ef0d403f13ec88e1cf3e3941ebc034
JK1320 <73619227+JK1320@users.noreply.github.com>
2021-02-19 11:39:51 +0000

    Level - 100

--- script.js
+++ script.js
@@ -6,7 +6,26 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  let container = document.getElementById("container")
+  let input = document.getElementById("filter");
+
+  input.addEventListener("keyup", (elem) => {
+    console.log(elem.target.value);
+  })
+
+  episodeList.forEach(episode => {
+    // let img = document.createElement("img");
+    // img.setAttribute("src", episode.image.medium)
+    // container.appendChild(img);
+    // container.innerHTML += `<h2>${episode.name} - Season: ${paddedToTwoDigits(episode.season)} - Episode: ${paddedToTwoDigits(episode.number)} </h2> {img} ${episode.summary}`;
+    container.innerHTML += `<h2>${episode.name} - Season: ${paddedToTwoDigits(episode.season)} - Episode: ${paddedToTwoDigits(episode.number)} </h2> <img src="${episode.image.medium}"/> ${episode.summary}`
+  })
+ 
 }
 
-window.onload = setup;
+function paddedToTwoDigits (num) {
+return num.toString().padStart(2, 0);
+};
+
+window.onload = setup; 
