692f215d3902e691d6e5dd2ac2d8a43248c93d50
JK1320 <kaurjus1@gmail.com>
2021-02-26 01:30:32 +0000

    Level - 200
    add CSS and fix level 300

--- script.js
+++ script.js
@@ -1,31 +1,85 @@
 //You can edit ALL of the code here
+let input = document.getElementById("filter");
+let container = document.getElementById("container");
+let select = document.getElementById("select-input")
+let option = document.getElementById("option");
+let episodesOption = document.getElementById("episodes-option")
+let rootDiv = document.getElementById("root");
+let allEpisodes;
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+ allEpisodes = getAllEpisodes();
+ makePageForEpisodes(allEpisodes);
+ input.addEventListener("input", search);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-  let container = document.getElementById("container")
-  let input = document.getElementById("filter");
-
-  input.addEventListener("keyup", (elem) => {
-    console.log(elem.target.value);
-  })
-
-  episodeList.forEach(episode => {
-    // let img = document.createElement("img");
-    // img.setAttribute("src", episode.image.medium)
-    // container.appendChild(img);
-    // container.innerHTML += `<h2>${episode.name} - Season: ${paddedToTwoDigits(episode.season)} - Episode: ${paddedToTwoDigits(episode.number)} </h2> {img} ${episode.summary}`;
-    container.innerHTML += `<h2>${episode.name} - Season: ${paddedToTwoDigits(episode.season)} - Episode: ${paddedToTwoDigits(episode.number)} </h2> <img src="${episode.image.medium}"/> ${episode.summary}`
-  })
  
+function makePageForEpisodes(episodeList) {
+  episodeList.forEach((episode) => {
+    container.innerHTML += `<div id="episodesDiv"> <div id="title-div"> <h2>${
+      episode.name
+    } - Season: ${paddedToTwoDigits(
+      episode.season
+    )} - Episode: ${paddedToTwoDigits(episode.number)} </h2> </div> <img id="img" src="${
+      episode.image.medium
+    }"/> ${episode.summary}<div>`;
+  });
+}
+ 
+function paddedToTwoDigits(num) {
+  return num.toString().padStart(2, 0);
 }
 
-function paddedToTwoDigits (num) {
-return num.toString().padStart(2, 0);
+// search function for input
+function search(elem) {
+  let searchString = elem.target.value.toLowerCase();
+  let filteredEpisodes = allEpisodes.filter((episode) => {
+    return (
+      episode.name.toLowerCase().includes(searchString) ||
+      episode.summary.toLowerCase().includes(searchString)
+    );
+   
+  });
+  container.innerHTML = "";
+  makePageForEpisodes(filteredEpisodes);
+}
+
+input.addEventListener("input", search);
+
+// function for select for episodes option
+function selectOption (elem) {
+
+  for(let i = 0; i < allEpisodes.length; i++) 
+  {
+    episodesOption.value = allEpisodes.name;
+    episodesOption.innerHTML = `${allEpisodes[i]}`;
+  }
+
+  select.innerHTML = episodesOption;
+
+    // select.forEach(episodes => {
+    //   episodesOption.value = episodes.name;
+    //   episodesOption.innerHTML = `${
+    //   episodes.name
+    // } - Season: ${paddedToTwoDigits(
+    //   episodes.season
+    // )} - Episode: ${paddedToTwoDigits(episodes.number)}`
+    // })
+
+    // select.addEventListener("change", (e) => {
+    //   let eachEpisode;
+    //   if(e.target.value === 0) {
+    //     eachEpisode = elem;
+    //   } 
+    // })
+    selectOption(allEpisodes);
 };
 
-window.onload = setup; 
+ 
+//  function paddedToTwoDigits(num) {
+//    if (num < 10) return "0" + number;
+//    else return num;
+//  }
+
+ 
+window.onload = setup;
